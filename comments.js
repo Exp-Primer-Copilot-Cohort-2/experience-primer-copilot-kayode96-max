53443bf3e613acba5c53a99988f9e4a37e4e53e5
Usman Abdullahi Olukayode <olukayodeusman406@gmail.com>
2024-08-16 03:17:33 +0000

    copilot third commit

---
+++ comments.js
@@ -0,0 +1,54 @@
+// create web server
+// create web server
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+    // read file
+    const filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
+    const extname = path.extname(filePath);
+    let contentType = 'text/html';
+
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+        case '.jpg':
+            contentType = 'image/jpg';
+            break;
+    }
+
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            if (err.code === 'ENOENT') {
+                // page not found
+                fs.readFile(path.join(__dirname, 'public', '404.html'), (err, content) => {
+                    res.writeHead(200, {'Content-Type': 'text/html'});
+                    res.end(content, 'utf8');
+                });
+            } else {
+                // some server error
+                res.writeHead(500);
+                res.end(`Server Error: ${err.code}`);
+            }
+        } else {
+            // success
+            res.writeHead(200, {'Content-Type': contentType});
+            res.end(content, 'utf8');
+        }
+    });
+});
+
+// create port
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));

db371ee7a597f95fc70d4c986fca68fa0dd01c28
Usman Abdullahi Olukayode <olukayodeusman406@gmail.com>
2024-08-16 02:42:58 +0000

    copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMember() {
+    var member = {
+        name: 'John',
+        age: 30,
+        skills: ['HTML', 'CSS', 'JS'],
+        greet: function() {
+            console.log('Hello, I am ' + this.name + ' and I am ' + this.age + ' years old.');
+        }
+    };
+
+    return member;
+}
